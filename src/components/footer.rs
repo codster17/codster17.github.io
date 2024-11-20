198bc3042f4e87d5b1541a9bc692190b2da5a4f3
andi-mache <amache75@gmail.com>
2024-11-20 11:31:36 +0300

    test two

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -4,7 +4,7 @@ use yew::prelude::*;
 pub fn footer() -> Html {
     html! {
         <footer class="p-4 text-center text-gray-600 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700 font-head">
-            {"\u{00a9} 2023 Copyright: Blog"}
+            {"\u{00a9} 2024 Copyright: Blog"}
         </footer>
     }
 }
